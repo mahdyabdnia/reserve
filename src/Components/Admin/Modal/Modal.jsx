3b8cc61b4b4c25450649a6c826ed5120cb70dee2
mahdyabdnia <44471104+mahdyabdnia@users.noreply.github.com>
2025-01-25 16:42:42 +0330

    gfG

--- src/Components/Admin/Modal/Modal.jsx
+++ src/Components/Admin/Modal/Modal.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import { Close } from '../../Icons/Icons'
+import React,{useState} from 'react'
+import { Close, Down } from '../../Icons/Icons'
 import useStyles from './styles'
 export default function Modal({ref}) {
     const classes=useStyles()
+
     const closeModal=(event)=>{
     const parent=event.currentTarget.parentElement.parentElement;
     parent.style.display="none"
@@ -11,6 +12,52 @@ export default function Modal({ref}) {
     <div className={classes.modal_root} ref={ref}>
         <div className={classes.modal_content}>
             <span className={classes.close_btn} onClick={closeModal}><Close/></span>
+            <div className={classes.modal_box}>
+              <form action="" onSubmit={handleForm}>
+                   <div className={classes.form_control}>
+                    <label htmlFor="" className={classes.form_label}>نام غذا</label>
+                    <div className={classes.form_input_box}>
+                      <input type="text" name="" id="" className={classes.form_input} />
+                    </div>
+                   </div>
+
+                   <div className={classes.form_control}>
+                    <label htmlFor="" className={classes.form_label}>توضیحات</label>
+                     
+                     <textarea name="" id="" cols="30" rows="10" className={classes.form_text}></textarea>
+                     
+                   </div>
+
+                   <div className={classes.form_control}>
+                    <label htmlFor="" className={classes.form_label}>وعده غذایی</label>
+                     
+                     <div className={classes.select_box}>
+                      <div className={classes.select}>
+                        <span>لطفا یک وعده غذایی را انتخاب کنید</span>
+                        <span><Down/></span>
+                      </div>
+
+                      <div className={classes.option_box}>
+                        <div className={classes.option}></div>
+                        
+                      </div>
+
+                     </div>
+                     
+                   </div>
+
+                   <div className={classes.form_control}>
+
+                    <label htmlFor="" className={classes.form_label}>قیمت غذا</label>
+                    <div className={classes.form_input_box}>
+                      <input type="text"  className={classes.form_input}/>
+                      <span>تومان</span>
+                    </div>
+                   </div>
+
+
+              </form>
+            </div>
         </div>
     </div>
   )
--- src/Components/Admin/Modal/styles.js
+++ src/Components/Admin/Modal/styles.js
@@ -41,6 +41,114 @@ close_btn:{
         fontSize:'20px',
         color:'black'
     }
+},
+modal_box:{
+    display:'flex',
+    flexDirection:'column',
+    padding:'1vh 1vw',
+    boxSizing:'border-box',
+    height:'auto',
+
+    
+},
+form_control:{
+    display:'flex',
+    flexDirection:'column',
+    rowGap:'20px',
+    alignItems:'flex-start',
+    justifyContent:'center',
+    boxSizing:'border-box',
+},
+form_label:{
+ display:'flex',
+ flexDirection:'row',
+ alignItems:'center',
+ justifyContent:'right',
+ fontSize:'15px',
+ color:'rgba(0,0,0,0.6)',
+ boxSizing:'border-box',
+ 
+},
+form_input_box:{
+display:'flex',
+flexDirection:'row',
+alignItems:'center',
+borderRadius:'5px',
+boxSizing:'border-box',
+padding:'1vmax 1vmax',
+border:'1px solid rgba(0,0,0,0.4)',
+
+
+},
+form_input:{
+display:'flex',
+flexDirection:'row',
+alignItems:'center',
+border:'none',
+outline:'none',
+width:'100%',
+color:'rgba(0,0,0,0.5)',
+'&:focus':{
+    border:'none',
+    outline:'none'
+}
+},
+form_text:{
+    display:'flex',
+    flexDirection:'row',
+    borderRadius:'5px',
+    border:'1px solid rgba(0,0,0,0.5)',
+    color:'black',
+    resize:'vertical',
+    wordBreak:'break-all',
+    lineHeight:'1.5',
+    boxSizing:'border-box',
+    width:'100%',
+    padding:'1vmax 1vmax'
+},
+select_box:{
+ display:'flex',
+ flexDirection:'column',
+ borderRadius:'5px',
+ border:'1px solid rgba(0,0,0,0.5)',
+ width:'100%',
+ position:'relative',
+ boxSizing:'border-box',
+ padding:'1vmax 1vmax'
+
+},
+select:{
+display:'flex',
+flexDirection:'row',
+alignItems:'center',
+justifyContent:'space-between',
+boxSizing:'border-box',
+padding:'1vmax 1vmax',
+fontSize:'15px',
+color:'rgba(0,0,0,0.8)'
+
+
+},
+option_box:{
+display:'flex',
+flexDirection:'column',
+postion:'absolute',
+boxSizing:'border-box',
+padding:'1vmax 1vmax',
+border:'1px solid rgba(0,0,0,0.5)',
+top:'100%',
+height:'auto',
+width:'100%',
+rowGap:'10px'
+
+},
+option:{
+ display:'flex',
+ flexDirection:'row',
+ alignItems:'center',
+ justifyContent:'space-between',
+ color:'rgba(0,0,0,0.7)',
+ fontSize:'15px'
 }
 })
 
